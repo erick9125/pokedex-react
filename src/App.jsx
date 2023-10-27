6667b19d6ac6d8acdfbfaf940d8bc9ece012bab0
Erick Morales <erick.morales@doctum.net>
2023-10-27 01:18:45 -0300

    Avance Menú y logica

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {AppRouter} from "./AppRouter"
+import { PokemonProvider } from "./context/PokemonProvider"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='App'></div>  
+    <PokemonProvider>
+      <AppRouter />
+    </PokemonProvider>  
   )
 }
 
---
+++ src/context/PokemonProvider.jsx
@@ -0,0 +1,10 @@
+import { PokemonContext } from "./PokemonContext"
+
+
+export const PokemonProvider = ({children}) => {
+    return (
+        <PokemonContext.Provider value={{numero: 0}}>
+            {children}
+        </PokemonContext.Provider>
+    )
+}
